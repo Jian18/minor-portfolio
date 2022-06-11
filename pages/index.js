b349d5ee1b859bfaece78c17176cb41a21c8f6b1
jian <jian.kootker@gmail.com>
2022-06-11 17:57:02 +0200

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Buttons from '../components/Buttons/Component';
 import Navigation from '../components/Navigation/Component';
 
-import Cirkel1 from '../public/assets/bolletjes/Cirkel1';
-import Cirkel2 from '../public/assets/bolletjes/Cirkel2';
-import Cirkel3 from '../public/assets/bolletjes/Cirkel3';
-import Cirkel4 from '../public/assets/bolletjes/Cirkel4';
-import Cirkel5 from '../public/assets/bolletjes/Cirkel5';
-import Cirkel6 from '../public/assets/bolletjes/Cirkel6';
-import Cirkel7 from '../public/assets/bolletjes/Cirkel7';
-import Cirkel8 from '../public/assets/bolletjes/Cirkel8';
+import {
+  Cirkel1,
+  Cirkel2,
+  Cirkel3,
+  Cirkel4,
+  Cirkel5,
+  Cirkel6,
+  Cirkel7,
+  Cirkel8,
+} from '../public/assets/bolletjes/cirkel';
 
 export default function Home() {
   return (
