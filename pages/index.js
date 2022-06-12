96aaa55ac4233dacc5f0d4a3728a7e8b5b32d643
jian <jian.kootker@gmail.com>
2022-06-12 18:31:42 +0200

    basis setup bolletjes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Buttons from '../components/Buttons/Component';
 import Navigation from '../components/Navigation/Component';
+import style from '/styles/Home.module.css';
 
 import {
   Cirkel1,
@@ -17,14 +18,16 @@ export default function Home() {
     <div>
       {/* <Navigation /> */}
       {/* <Buttons /> */}
-      <Cirkel1 />
-      <Cirkel2 />
-      <Cirkel3 />
-      <Cirkel4 />
-      <Cirkel5 />
-      <Cirkel6 />
-      <Cirkel7 />
-      <Cirkel8 />
+      <div className={style.background}>
+        <Cirkel1 className={style.cirkel1} />
+        <Cirkel2 className={style.cirkel2} />
+        <Cirkel3 className={style.cirkel3} />
+        <Cirkel4 className={style.cirkel4} />
+        <Cirkel5 className={style.cirkel5} />
+        <Cirkel6 className={style.cirkel6} />
+        <Cirkel7 className={style.cirkel7} />
+        <Cirkel8 className={style.cirkel8} />
+      </div>
     </div>
   );
 }
