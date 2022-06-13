959208e77641cb41e01b4a565db5aad9535e9f9d
DiamK <diam96@gmail.com>
2022-06-13 14:05:16 +0200

    nieuwe personen pagina

--- pages/personen/index.js
+++ pages/personen/index.js
@@ -1,34 +1,25 @@
-import React from 'react'
-import Buttons from '../../components/Buttons/Component'
+import React from 'react';
+import Buttons from '../../components/Buttons/Component';
 
 function PersonPage() {
   return (
     <div>
-      <Buttons buttonText="Vorige"/>
-      <Buttons buttonText="Volgende"/>
-    <div>
-      Image
-    </div> 
-    <div>
-      Naam {/* Hier komt naam props  */}
-    </div> 
+      <Buttons buttonText="Vorige" />
+      <Buttons buttonText="Volgende" />
+      <div>Image</div>
+      <div>Naam {/* Hier komt naam props  */}</div>
 
-
-        <div>
-          <h6>Front-end Developer</h6>
-          <ul>
-            <li>Project 1</li>
-            <li>Project 2</li>
-            <li>Project 3</li>
-          </ul>
-          <Buttons buttonText="Lees meer"/>
-        </div>
-      
-
-
+      <div>
+        <h6>Front-end Developer</h6>
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+          <li>Project 3</li>
+        </ul>
+        <Buttons buttonText="Lees meer" />
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default PersonPage
+export default PersonPage;
