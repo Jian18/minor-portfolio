0a57b4fab4a8476bef257a9df9bb23007a7722cd
jian <jian.kootker@gmail.com>
2022-06-15 13:55:55 +0200

    sanity setup basic

---
+++ sanityminor/schemas/personen.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'personen',
+  type: 'document',
+  title: 'Personen',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+  ],
+};
