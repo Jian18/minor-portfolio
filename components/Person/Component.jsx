afed30c849837dde5b9537c1caca23d71b23401f
jian <jian.kootker@gmail.com>
2022-06-16 01:52:44 +0200

    buttons toegevoegd

--- components/Person/Component.jsx
+++ components/Person/Component.jsx
@@ -8,34 +8,36 @@ import { Cirkels } from '../Cirkels/Component';
 
 export function Person({ person: { name, role, image, Projecten }, next, prev }) {
   return (
-    <div className={style.container}>
-      {/* <NextButton buttonText="Volgende" icon="FiArrowRightCircle " /> */}
-      {/* <PreviousButton buttonText="Vorige" icon="FiArrowLeftCircle" /> */}
+    <>
+      <div className={style.container}>
+        <PreviousButton buttonText="Vorige" icon="FiArrowLeftCircle" />
 
-      {/* <Image alt={image.title} src={`${image._ref}`} layout="fill" />
+        {/* <Image alt={image.title} src={`${image._ref}`} layout="fill" />
       {console.log(image, 'image')} */}
-      <div className={style.nameContainer}>
-        <img alt={urlFor(image)} src={urlFor(image)} className={style.image} />
-        <div className={style.nameBackground}>
-          <h3 className={style.name}>{name}</h3>
+        <div className={style.nameContainer}>
+          <img alt={urlFor(image)} src={urlFor(image)} className={style.image} />
+          <div className={style.nameBackground}>
+            <h3 className={style.name}>{name}</h3>
+          </div>
         </div>
-      </div>
 
-      <div className={style.glass}>
-        <div className={style.glassText}>
-          <h6 className={style.role}>{role}</h6>
-          <ul>
-            {Projecten.map((project) => (
-              <li key={project._key} className={style.projectList}>
-                project titel
-              </li>
-            ))}
-          </ul>
+        <div className={style.glass}>
+          <div className={style.glassText}>
+            <h6 className={style.role}>{role}</h6>
+            <ul>
+              {Projecten.map((project) => (
+                <li key={project._key} className={style.projectList}>
+                  project titel
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={style.glassButton}>
+            <ReadMore buttonText="Lees meer" />
+          </div>
         </div>
-        <div className={style.glassButton}>
-          <ReadMore buttonText="Lees meer" />
-        </div>
+        <NextButton buttonText="Volgende" icon="FiArrowRightCircle " />
       </div>
-    </div>
+    </>
   );
 }
