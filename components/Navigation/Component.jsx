87f1650d79cbdfbfcc627ac531b2a93e1daa60c8
DiamK <diam96@gmail.com>
2022-06-27 11:18:47 +0200

    fix menu

--- components/Navigation/Component.jsx
+++ components/Navigation/Component.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import style from './Navigation.module.css';
 import Link from 'next/link';
 import { FiMenu, FiX } from 'react-icons/fi';
+import { Logo } from '../../public/assets/Logo';
 
 export function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
 
-  // if person == setPerson{ set className = {style.selectedName}}
   return (
     <div className={style.container}>
+      <Logo className={style.logo} />
       <button className={style.button} onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <FiX className={style.icon} /> : <FiMenu className={style.icon} />}
       </button>
       {isOpen && (
-        <nav className={style.nav}>
+        <nav className={style.nav} onClick={() => setIsOpen(!isOpen)}>
           <ul>
             <li className={style.li}>
               <Link href="/projectOverzicht/diam-kanbier">
