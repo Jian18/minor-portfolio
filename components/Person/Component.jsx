ffc7b5ca5464b1981e0ccba404ef9e8d94027702
jian <jian.kootker@gmail.com>
2022-06-29 01:47:43 +0200

    Slider echt fix

--- components/Person/Component.jsx
+++ components/Person/Component.jsx
@@ -27,47 +27,49 @@ export function Person({ person: { name, role, image, Projecten }, next, prev })
     },
   };
   return (
-    <motion.div
-      exit={{ opacity: 0 }}
-      initial="initial"
-      animate="animate"
-      variants={content}
-      key={slug}
-      className={style.container}
-    >
-      <PreviousButton buttonText="Vorige" icon="FiArrowLeftCircle" buttonLink={prev} />
-      <motion.div variants={content} className={style.childContainer}>
-        <div className={style.nameContainer}>
-          <picture>
-            <source srcSet={urlFor(image).url()} type="image" />
-            <img alt={image.name} src={urlFor(image).url()} className={style.image} />
-          </picture>
-          <div className={style.nameBackground}>
-            <h3 className={style.name}>{name}</h3>
+    <>
+      <motion.div
+        exit={{ opacity: 0 }}
+        initial="initial"
+        animate="animate"
+        variants={content}
+        key={slug}
+        className={style.container}
+      >
+        <PreviousButton buttonText="Vorige" icon="FiArrowLeftCircle" buttonLink={prev} />
+        <motion.div variants={content} className={style.childContainer}>
+          <div className={style.nameContainer}>
+            <picture>
+              <source srcSet={urlFor(image).url()} type="image" />
+              <img alt={image.name} src={urlFor(image).url()} className={style.image} />
+            </picture>
+            <div className={style.nameBackground}>
+              <h3 className={style.name}>{name}</h3>
+            </div>
           </div>
-        </div>
-        <div exit={{ opacity: 0 }} className={style.glass}>
-          <div className={style.glassText}>
-            <h6 className={style.role}>{role}</h6>
-            <ul>
-              {Projecten.map((project) => (
-                <li key={project._id} className={style.projectList}>
-                  <span className={style.checkIcon}>
-                    <FiCheck />
-                  </span>
-                  {project.projectNaam}
-                </li>
-              ))}
-            </ul>
+          <div exit={{ opacity: 0 }} className={style.glass}>
+            <div className={style.glassText}>
+              <h6 className={style.role}>{role}</h6>
+              <ul>
+                {Projecten.map((project) => (
+                  <li key={project._id} className={style.projectList}>
+                    <span className={style.checkIcon}>
+                      <FiCheck />
+                    </span>
+                    {project.projectNaam}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={style.glassButton}>
+              <ReadMore buttonText="Lees meer" buttonLink={`/projectOverzicht/${slug}`} />
+            </div>
           </div>
-          <div className={style.glassButton}>
-            <ReadMore buttonText="Lees meer" buttonLink={`/projectOverzicht/${slug}`} />
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
 
-      <NextButton buttonText="Volgende" icon="FiArrowRightCircle" buttonLink={next} />
+        <NextButton buttonText="Volgende" icon="FiArrowRightCircle" buttonLink={next} />
+      </motion.div>
       <div className={`patroon ${style.patroon}`} />
-    </motion.div>
+    </>
   );
 }
